e25d7045ec8e840e8d04d35ec84ee29bea18b36d
dige <161250087@mail.smal.nju.edu.cn>
2019-05-23 15:16:04 +0800

    1

--- test/src/test/java/com/test/test/TestApplicationTests.java
+++ test/src/test/java/com/test/test/TestApplicationTests.java
@@ -1,24 +1,19 @@
 package com.test.test;
 
-import com.test.test.entity.User;
-import com.test.test.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestApplicationTests {
-    @Autowired
-    private UserService userService;
 
     @Test
     public void test() throws Exception{
-        User u = new User("123","456","madi2009@126.com");
+        /*User u = new User("123","456","madi2009@126.com");
         int an =userService.loginConfig(u);
         if(an>0) System.out.print("FFFF");
-        else System.out.print("TTTT");
+        else System.out.print("TTTT");*/
     }
 }
