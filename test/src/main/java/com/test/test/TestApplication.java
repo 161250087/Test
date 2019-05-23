c5c8ade9afd0324dd8725d059b4c50e2b8406509
dige <161250087@mail.smal.nju.edu.cn>
2019-05-23 13:54:10 +0800

    1

--- test/src/main/java/com/test/test/TestApplication.java
+++ test/src/main/java/com/test/test/TestApplication.java
@@ -2,10 +2,13 @@ package com.test.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@ComponentScan(basePackages ="com.test.test.entity")
 public class TestApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
     }
