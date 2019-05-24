683bffd8d233cc74fd71593671dbab638910a9f4
dige <161250087@mail.smal.nju.edu.cn>
2019-05-24 14:21:30 +0800

    2

--- test/src/test/java/com/test/test/TestApplicationTests.java
+++ test/src/test/java/com/test/test/TestApplicationTests.java
@@ -1,13 +1,17 @@
 package com.test.test;
 
+import com.test.test.service.MailService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestApplicationTests {
+    @Autowired
+    public MailService mailService;
 
     @Test
     public void test() throws Exception{
@@ -15,5 +19,10 @@ public class TestApplicationTests {
         int an =userService.loginConfig(u);
         if(an>0) System.out.print("FFFF");
         else System.out.print("TTTT");*/
+    }
+
+    @Test
+    public void test1() throws  Exception{
+        mailService.sendSimpleMail("1293086146@qq.com","FUCK","FUCK");
     }
 }
