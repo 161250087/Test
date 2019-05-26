14c72d2ea43093cdaa92c369072a8830eaaefc58
161250133 <1024088621@qq.com>
2019-05-26 16:52:21 +0800

    test

--- test/src/main/java/com/test/demo/entity/Collection.java
+++ test/src/main/java/com/test/demo/entity/Collection.java
@@ -33,7 +33,7 @@ public class Collection implements Serializable{
         this.article_id = article_id;
     }
 
-    public void setUser_id(String tag) {
+    public void setUser_id(int user_id) {
         this.user_id = user_id;
     }
 
--- test/src/main/java/com/test/demo/entity/Subscribe.java
+++ test/src/main/java/com/test/demo/entity/Subscribe.java
@@ -18,22 +18,6 @@ public class Subscribe implements Serializable {
     @Column(name="author",nullable = false)
     private String author;
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setUser_id(String tag) {
-        this.user_id = user_id;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public int getUser_id() {
-        return user_id;
-    }
-
     @Column(name="user_id",nullable = false)
     private int user_id;
 
@@ -43,5 +27,21 @@ public class Subscribe implements Serializable {
     public Subscribe(String author,int user_id){
         this.author=author;
         this.user_id=user_id;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getUser_id() {
+        return user_id;
     }
 }
